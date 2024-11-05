66a5867dd8456f4f8af9b98a7e1b744da8245a95
MlodziutkiS <zuparek@gmail.com>
2024-11-05 21:14:00 +0100

    adding video box and video.js

--- my-app/src/components/MovieBox/MovieBox.jsx
+++ my-app/src/components/MovieBox/MovieBox.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
-import '@vidstack/react/player/styles/base.css';
-function MovieBox(){
-    return(
-        <div>
+import React, { useState } from 'react';
+import './MovieBox.css';
+import videos from '../../database/videos';
+import { preload } from 'react-dom';
 
+function MovieBox(){
+    const [vidId, setvidId]=useState(0);
+    let maxId = videos.length-1;
+    //console.log(videos[vidId+1]);
+    function handlePreload(){
+        /* 
+        ## awaiting {as: "video"} to become a valid value
+        ## look for /react-dom/cjs/react-dom-client.development.js changes
+        try{
+            preload(videos[vidId+1].link, {as: "video"});
+            preload(videos[videos.length-1].link, {as: "video"});
+            preload(videos[vidId-1].link, {as: "video"});
+        }
+        catch{
+            //console.log("womp womp preload didnt preload something");
+            return;
+        }*/
+	}
+    return(
+        <div className='videoBox'>
+            <button onClick={() => vidId===0? setvidId(maxId):setvidId(vidId-1)}>&lt;</button>
+            <video width="50%" height="auto" controls key={vidId} onCanPlay={handlePreload()}>
+                <source src={videos[vidId].link} type="video/mp4"/>
+            Your browser does not support the video tag.<br/>description {videos[vidId].desc}
+            </video>
+            <button onClick={() => vidId===maxId? setvidId(1):setvidId(vidId+1)}>&gt;</button>
         </div>
     );
 }
---
+++ my-app/src/database/videos.js
@@ -0,0 +1,20 @@
+const videos=[{
+    link:"/video/5bieg.mp4",
+    desc:"Start z 5 biegu"
+},{
+    link:"/video/dziadek.mp4",
+    desc:"Dziadek trzyma 6k obrotów"
+},{
+    link:"/video/fordpalety.mp4",
+    desc:"Wirtuozi parkingów"
+},{
+    link:"/video/gumawies.mp4",
+    desc:"Oporowe palenie lacza"
+},{
+    link:"/video/pimp.mp4",
+    desc:"Fura renata"
+},{
+    link:"/video/skok.mp4",
+    desc:"Skok bandziorem"
+}]
+export default videos
