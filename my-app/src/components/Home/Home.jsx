3472ac70943b4d089d0d08fac90c9b5f8a677e71
MlodziutkiS <zuparek@gmail.com>
2024-11-05 18:02:33 +0100

    minor css/styling Changes

--- my-app/src/components/Home/Home.jsx
+++ my-app/src/components/Home/Home.jsx
@@ -1,4 +1,3 @@
-import './Home.css';
 function Home(){
     return <div className='Home'>
         <img src='./oelyx.png'/>
--- my-app/src/components/Pages/Navi.jsx
+++ my-app/src/components/Pages/Navi.jsx
@@ -1,14 +1,17 @@
 import Home from "../Home/Home";
 import Button from "../Button/Button";
 import { Link } from "react-router-dom";
+import './Navi.css';
 function Navi(){
     return (
         <div className='banner'>
             <Link to="/"><Home/></Link>
-            <Link to="/muza"><Button text="muza" style="olx"/></Link>
-            <Link to="/filmy"><Button text="Kinematografia" style="olx"/></Link>
-            <Link to="/kontakt"><Button text="Info" style="olx"/></Link>
-            <Link to="/game"><Button text="Przegladaj gruzy" style="olx"/></Link>
+            <div className="naviButton">
+                <Link to="/muza"><Button text="muza" style="olx"/></Link>
+                <Link to="/filmy"><Button text="Kinematografia" style="olx"/></Link>
+                <Link to="/kontakt"><Button text="Info" style="olx"/></Link>
+                <Link to="/game"><Button text="Przegladaj gruzy" style="olx"/></Link>
+            </div>
         </div>
     );
 }
